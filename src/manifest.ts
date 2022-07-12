4f0dd9fe5c0daaca044b35ddc82ad53e86373f92
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2022-07-12 17:43:25 +0200

    Change manifest to relative urls

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,28 +1,29 @@
 // a melonJS data manifest
 // note : this is note a webpack manifest
 const DataManifest = [
+  { name: 'area01', type: 'tmx', src: 'data/map/area01.tmx' },
+  { name: 'area02', type: 'tmx', src: 'data/map/area02.tmx' },
   /* Bitmap Text */
   {
     name: 'PressStart2P',
     type: 'image',
-    src: './data/fnt/PressStart2P.png'
+    src: 'data/fnt/PressStart2P.png'
   },
   {
     name: 'PressStart2P',
     type: 'binary',
-    src: './data/fnt/PressStart2P.fnt'
+    src: 'data/fnt/PressStart2P.fnt'
   },
   {
     name: 'area01_level_tiles',
     type: 'image',
-    src: './data/img/map/area01_level_tiles.png'
+    src: 'data/img/map/area01_level_tiles.png'
   },
   {
     name: 'gripe_run_right',
     type: 'image',
-    src: './data/img/sprite/gripe_run_right.png'
-  },
-  { name: 'area01', type: 'tmx', src: './data/map/area01.tmx' }
+    src: 'data/img/sprite/gripe_run_right.png'
+  }
 ]
 
 export default DataManifest
