168f02b371c8826de01f948ed337140c12370e3f
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2022-07-14 14:24:12 +0200

    Remove WC errors

--- src/config/connectors.ts
+++ src/config/connectors.ts
@@ -1,11 +1,7 @@
 import { Web3Provider } from '@ethersproject/providers'
-import WalletConnectProvider from '@walletconnect/web3-provider'
+// import WalletConnectProvider from '@walletconnect/web3-provider'
 
 import { CHAIN_ID, JSON_RPC_PROVIDER } from '../config'
-
-// type AbstractConnector = NonNullable<
-//   ReturnType<typeof Web3Provider>['connector']
-// >
 
 const currentRpcString = JSON_RPC_PROVIDER as string
 
@@ -17,11 +13,14 @@ export const injected = new Web3Provider(
   (window as any).ethereum // Haaaaacky
 )
 
+// Duplicate to keep TS happy
 export const walletconnect = new Web3Provider(
-  new WalletConnectProvider({ rpc: activeRpcUrl[CHAIN_ID] })
+  (window as any).ethereum // Haaaaacky
 )
 
-// use "provider.enable()"" to trigger qr
+// export const walletconnect = new Web3Provider(
+//   new WalletConnectProvider({ rpc: activeRpcUrl[CHAIN_ID] })
+// )
 
 export const walletsToConnectors = {
   metamask: injected,
@@ -29,16 +28,3 @@ export const walletsToConnectors = {
 }
 
 export type Wallets = keyof typeof walletsToConnectors
-
-// export const walletByConnector = (
-//   connector: AbstractConnector
-// ): Wallets | null => {
-//   switch (true) {
-//     case connector instanceof Web3Provider:
-//       return 'metamask'
-//     case connector instanceof walletconnect:
-//       return 'walletConnect'
-//     default:
-//       return null
-//   }
-// }
