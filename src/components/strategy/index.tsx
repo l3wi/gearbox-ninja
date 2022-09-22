42b48b613c2c0c06dd6fe11aceae9be97cda3b42
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2022-09-22 12:04:59 +0200

    Integrate picker

--- src/components/strategy/index.tsx
+++ src/components/strategy/index.tsx
@@ -47,6 +47,7 @@ import { useHF } from '../../hooks/useHF'
 import { useOverallAPY } from '../../hooks/useCreditAccounts'
 import Picker from './picker'
 import { AssetWithView } from '../../config/asset'
+import { nFormatter } from '../../utils/format'
 
 const getStrategy = (state: RootState) => {
   const { symbol } = state.form
@@ -160,7 +161,6 @@ const Form = () => {
     lpTokenAddress
   )
   const assetsAfterOpen = strategyPath?.balances || EMPTY_ARRAY
-
   const hfFrom =
     useHF({
       assets: assetsAfterOpen,
@@ -202,6 +202,8 @@ const Form = () => {
       BigNumber.from(0)
     )
   }, [lpTokenAddress, assetsAfterOpen])
+
+  console.log(lpAmount, hfFrom)
 
   const liquidationPrice = strategy.liquidationPrice({
     assets: wrappedCollateral,
@@ -245,17 +247,17 @@ const Form = () => {
   //   }
   // }
 
-  const readableBalance =
-    asset && balances[asset.address]
-      ? balances[asset.address]
-          .div(BigNumber.from('10').pow(BigNumber.from(asset.decimals)))
-          .toString()
-      : '0'
+  // const readableBalance =
+  //   asset && balances[asset.address]
+  //     ? balances[asset.address]
+  //         .div(BigNumber.from('10').pow(BigNumber.from(asset.decimals)))
+  //         .toString()
+  //     : '0'
 
-  const max = () => {
-    updateValue(readableBalance)
-    setMax(true)
-  }
+  // const max = () => {
+  //   updateValue(readableBalance)
+  //   setMax(true)
+  // }
 
   const handleSubmit = () => {}
   // const handleOpenClick: OpenStrategyManagerProps["onOpenClick"] = props => {
@@ -302,9 +304,14 @@ const Form = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [provider, prices])
 
+  // Reset Picker on select
+  useEffect(() => {
+    setPicker(false)
+  }, [unwrappedCollateral])
+
   // index 0
-  const updateValue = (input: string) => {
-    const func = handleChangeAmount(0)
+  const updateValue = (index: number, input: string) => {
+    const func = handleChangeAmount(index)
     const token = tokensList[unwrappedCollateral[0].token]
     if (!input || input.match(/^\d{1,}(\.\d{0,4})?$/)) {
       const bn = BigNumber.from(input).mul(
@@ -318,90 +325,131 @@ const Form = () => {
     <FormBg>
       <Underground>
         <ExitButton onClick={() => exit()}>✕</ExitButton>
+        <h1>
+          {`Invest in ${symbol.toUpperCase()} `}
+          <img
+            width={30}
+            src={`https://static.gearbox.fi/tokens/${symbol.toLowerCase()}.svg`}
+          />
+        </h1>
+        <Row>
+          <FormContainer>
+            <h3>Deposit Assets</h3>
+            {unwrappedCollateral.map((collateral, i) => {
+              return (
+                <Section>
+                  <InputSuper>
+                    <span>
+                      {`BALANCE: 
+                        ${nFormatter(
+                          balances[collateral.token],
+                          tokensList[collateral.token].decimals,
+                          3
+                        )} 
+                        ${
+                          tokensList[collateral.token] &&
+                          tokensList[collateral.token].symbol.toUpperCase()
+                        }`}
+                    </span>
+                    <span />
+                  </InputSuper>
+                  <InputGroup>
+                    <Input
+                      placeholder="0.0"
+                      value={collateral.balanceView}
+                      onChange={(e) => updateValue(i, e.target.value)}
+                    />
 
-        <FormContainer>
-          <h2>
-            {`Invest in ${symbol.toUpperCase()} `}
-            <img
-              width={30}
-              src={`https://static.gearbox.fi/tokens/${symbol.toLowerCase()}.svg`}
-            />
-          </h2>
-          <InputSuper>
-            <span>{`DEPOSIT`}</span>
-            <span>
-              {`BALANCE: 
-              ${new Intl.NumberFormat('en-US', {
+                    <Asset>
+                      <>
+                        <img
+                          width={20}
+                          src={tokensList[collateral.token].icon}
+                        />
+                        <span>
+                          {tokensList[collateral.token].symbol.toUpperCase()}
+                        </span>
+                      </>
+                    </Asset>
+                    <MaxButton
+                      onClick={() =>
+                        updateValue(i, balances[collateral.token].toString())
+                      }
+                    >
+                      max
+                    </MaxButton>
+                  </InputGroup>
+                </Section>
+              )
+            })}
+
+            <Group>
+              <>
+                <PickerButton onClick={() => setPicker(true)}>
+                  add asset
+                </PickerButton>
+                {picker && (
+                  <Picker
+                    selected={unwrappedCollateral}
+                    tokensList={tokensList}
+                    balances={balancesWithETH}
+                    allowedTokens={allowedTokens}
+                    addAsset={handleAdd}
+                  />
+                )}
+              </>
+            </Group>
+            <Group>
+              <span>Borrowed Asset: </span>
+              <Asset>
+                <img
+                  width={20}
+                  src={`https://static.gearbox.fi/tokens/${underlyingToken.symbol.toLowerCase()}.svg`}
+                />
+                <span>{underlyingToken.symbol.toUpperCase()}</span>
+              </Asset>
+            </Group>
+          </FormContainer>
+          <FormContainer>
+            <Group>
+              <Slider
+                amount={totalAmount}
+                minAmount={minAmount}
+                maxAmount={maxAmount}
+                leverage={leverage}
+                maxLeverage={maxLeverageFactor}
+                setLeverage={setLeverage}
+              />
+            </Group>
+            <Group>
+              <span>You'll recieve</span>
+              <span>{`${new Intl.NumberFormat('en-US', {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 8
-              }).format(parseFloat(readableBalance))} 
-              ${asset.symbol.toUpperCase()}`}
-            </span>
-          </InputSuper>
-          <InputGroup>
-            <Input
-              value={unwrappedCollateral[0].balanceView}
-              onChange={(e) => updateValue(e.target.value)}
-            />
-            <Dropdown
-              asset={tokensList[unwrappedCollateral[0].token]}
-              list={unwrappedCollateral}
-              tokens={tokensList}
-              func={setAsset}
-            />
-            <MaxButton onClick={() => max()}>max</MaxButton>
-          </InputGroup>
-          {/* <Group>
-            <>
-              <button onClick={() => setPicker(true)}>add asset</button>
-              {picker && (
-                <Picker
-                  tokensList={tokensList}
-                  balances={balancesWithETH}
-                  allowedTokens={allowedTokens}
-                />
-              )}
-            </>
-          </Group> */}
-          <Group>
-            <span>Borrowed Asset: </span>
-            <Asset>
-              <img
-                width={20}
-                src={`https://static.gearbox.fi/tokens/${underlyingToken.symbol.toLowerCase()}.svg`}
-              />
-              <span>{underlyingToken.symbol.toUpperCase()}</span>
-            </Asset>
-          </Group>
-          <Group>
-            <Slider
-              amount={totalAmount}
-              minAmount={minAmount}
-              maxAmount={maxAmount}
-              leverage={leverage}
-              maxLeverage={maxLeverageFactor}
-              setLeverage={setLeverage}
-            />
-          </Group>
-          <Group>
-            <span>You'll recieve</span>
-            <span>{`${
-              (parseFloat(assetsAfterOpen.toString()) * leverage) /
-              LEVERAGE_DECIMALS
-            } ${lpSymbol.toUpperCase()}`}</span>
-          </Group>
-          <Group>
-            <span>Strategy APY</span>
-            <span>{overallAPYFrom}%</span>
-          </Group>
-          <SubmitButton
-            // disabled={disableSubmit()}
-            onClick={() => handleSubmit()}
-          >
-            {true ? 'Insufficent Balance' : 'deposit'}
-          </SubmitButton>
-        </FormContainer>
-        <span />
+              }).format(
+                parseFloat(
+                  lpAmount
+                    .div(BigNumber.from('10').pow(BigNumber.from(18)))
+                    .toString()
+                )
+              )} ${lpSymbol.toUpperCase()}`}</span>
+            </Group>
+            <Group>
+              <span>Strategy APY</span>
+              <span>{overallAPYFrom}%</span>
+            </Group>
+            <Group>
+              <span>Health Factor</span>
+              <span>{hfFrom / 10000}</span>
+            </Group>
+            <SubmitButton
+              // disabled={disableSubmit()}
+              onClick={() => handleSubmit()}
+            >
+              {true ? 'Insufficent Balance' : 'deposit'}
+            </SubmitButton>
+          </FormContainer>
+        </Row>
       </Underground>
     </FormBg>
   )
@@ -427,56 +475,6 @@ function useLiquidationAssets(
   return liquidationAssets
 }
 
-const Dropdown: React.FC<{
-  asset: TokenData
-  list: AssetWithView[]
-  tokens: Record<string, TokenData>
-  func: any
-}> = ({ asset, list, func, tokens }) => {
-  const [isOpen, setOpen] = useState(false)
-
-  const select = (item: TokenData) => {
-    func(item)
-    setOpen(!isOpen)
-  }
-
-  return (
-    <div>
-      <Row onClick={() => setOpen(!isOpen)}>
-        <Asset>
-          {asset && (
-            <>
-              <img
-                width={20}
-                src={`https://static.gearbox.fi/tokens/${asset.symbol.toLowerCase()}.svg`}
-              />
-              <span>{asset.symbol.toUpperCase()}</span>
-            </>
-          )}
-        </Asset>
-        <Down>⌄</Down>
-      </Row>
-      <Col>
-        {isOpen &&
-          list.map((item) => (
-            <Asset
-              key={tokens[item.token].address}
-              onClick={() => select(tokens[item.token])}
-            >
-              <img
-                width={20}
-                src={`https://static.gearbox.fi/tokens/${tokens[
-                  item.token
-                ].symbol.toLowerCase()}.svg`}
-              />
-              <span>{tokens[item.token].symbol.toUpperCase()}</span>
-            </Asset>
-          ))}
-      </Col>
-    </div>
-  )
-}
-
 const Group = styled.div`
   display: flex;
   justify-content: space-between;
@@ -486,8 +484,27 @@ const Group = styled.div`
   width: 100%;
 `
 
-const Content = styled.div`
-  max-width: 400px;
+const PickerButton = styled.button`
+  width: 100%;
+  color: rgba(255, 255, 255, 0.5);
+  background: transparent;
+  padding: 15px 8px;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: 800;
+  text-transform: uppercase;
+  font-size: 15px;
+  margin: 0px;
+  font-family: 'Press Start 2P';
+  border: 2px solid rgba(255, 255, 255, 0.5);
+  transition: 0.1s ease-in-out;
+  &:hover {
+    border: 2px solid rgba(255, 255, 255, 1);
+    color: rgba(255, 255, 255, 1);
+  }
+`
+
+const Section = styled.div`
+  padding: 10px 0px;
 `
 
 const InputSuper = styled.div`
@@ -512,7 +529,7 @@ const Input = styled.input`
   outline: none;
   background: none;
   color: white;
-  width: 130px;
+  width: 150px;
   padding: 5px 8px 0px;
   font-size: 18px;
 `
@@ -531,7 +548,6 @@ const MaxButton = styled.div`
 `
 
 const FormContainer = styled.div`
-  margin-top: 65px;
   font-size: 18px;
   width: 100%;
   max-width: 350px;
@@ -550,7 +566,6 @@ const SubmitButton = styled.button`
   font-size: 20px;
   margin: 0px;
   font-family: 'Press Start 2P';
-  z-index: 50;
 `
 
 const ExitButton = styled.button`
@@ -567,7 +582,8 @@ const ExitButton = styled.button`
 const Underground = styled.div`
   position: relative;
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   min-width: 1023px;
   min-height: 512px;
@@ -605,7 +621,6 @@ const Down = styled.span`
 `
 const Row = styled.span`
   display: flex;
-  height: 100%;
   align-items: center;
 `
 const Col = styled.span`
--- src/components/strategy/picker.tsx
+++ src/components/strategy/picker.tsx
@@ -8,31 +8,88 @@ import {
   LEVERAGE_DECIMALS,
   TokenBalance
 } from '@gearbox-protocol/sdk'
+import { bnToFloat, nFormatter } from '../../utils/format'
+import { AssetWithView } from '../../config/asset'
+
+const sortBalanceByAddress = (
+  tokens: string[],
+  balances: { [x: string]: BigNumberish },
+  tokensList: Record<string, TokenData>
+) => {
+  return tokens
+    .filter((i) => tokensList[i])
+    .sort(
+      (a, b) =>
+        bnToFloat(balances[a], tokensList[a].decimals) -
+        bnToFloat(balances[b], tokensList[b].decimals)
+    )
+    .reverse()
+}
 
 const Picker: React.FC<{
+  selected: AssetWithView[]
   allowedTokens: string[]
   balances: { [x: string]: BigNumberish }
   tokensList: Record<string, TokenData>
-}> = ({ allowedTokens = [], balances, tokensList }) => {
+  addAsset: (tokenAddress: string) => void
+}> = ({ selected, allowedTokens = [], balances, tokensList, addAsset }) => {
+  const list = allowedTokens.filter(
+    (item) => !selected.map((i) => i.token).includes(item)
+  )
+
   return (
-    <div>
-      {allowedTokens.map((item) => {
-        const token = tokensList[item]
-        if (token)
+    <Container>
+      <List>
+        {sortBalanceByAddress(list, balances, tokensList).map((item) => {
+          const token = tokensList[item]
           return (
-            <>
-              <div>{token.symbol}</div>
-              <div>{balances[token.address].toString()}</div>
-            </>
+            <Item
+              onClick={() => addAsset(token.address)}
+              key={'list-' + token.symbol}
+            >
+              <Row>
+                <img src={token.icon} width="20" />
+                <div style={{ paddingLeft: 5 }}>{token.symbol}</div>
+              </Row>
+              <div>
+                {nFormatter(balances[token.address], token.decimals, 2)}
+              </div>
+            </Item>
           )
-      })}
-    </div>
+        })}
+      </List>
+    </Container>
   )
 }
 
+const Container = styled.div`
+  position: absolute;
+  z-index: 200;
+  width: 100%;
+  max-width: 300px;
+  padding: 20px 30px;
+  background-color: black;
+`
+const List = styled.div`
+  width: 100%;
+  max-height: 400px;
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3px 5px;
+`
+
 const Row = styled.span`
   display: flex;
-  height: 100%;
   align-items: center;
 `
 const Col = styled.span`
