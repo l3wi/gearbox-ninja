cc60dc6f154810b90ee8d7554f4e4aca05debabb
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2022-10-31 11:04:45 +0100

    Hide logging

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,21 +3,31 @@ import thunk from "redux-thunk";
 
 import reducer from "./reducer";
 import acts from "./actions";
+import { IS_TEST_NETWORK } from "../config";
 export type RootState = ReturnType<typeof reducer>;
+
+let isLogging = false;
 
 // Ghetto logger
 const logger: Middleware<{}, RootState> = store => next => action => {
   // Skipp logger for notifications
   if (action.type === "UPDATE_NOTIFICATION") return next(action);
-  console.group(action.type);
-  console.info("dispatching", action);
-  let result = next(action);
-  console.log("next state", store.getState());
-  console.groupEnd();
-  return result;
+  if (isLogging || IS_TEST_NETWORK) {
+    console.group(action.type);
+    console.info("dispatching", action);
+    let result = next(action);
+    console.log("next state", store.getState());
+    console.groupEnd();
+    return result;
+  }
+  return next(action);
 };
 
 export function configureStore() {
+  window.logging = function () {
+    console.log("Starting logging");
+    isLogging = !isLogging;
+  };
   return createStore(reducer, applyMiddleware(thunk, logger));
 }
 
