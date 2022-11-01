88eb900e742b8b6c0ad6fb379211769c902c5d4c
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2022-11-01 11:36:04 +0100

    Fix: Stop track doubling after interaction with MM

--- src/store/game/actions.tsx
+++ src/store/game/actions.tsx
@@ -82,6 +82,13 @@ export const ChangeStage =
         if (currentStage === "MENU" && key === "PLAY" && track)
           audio.playTrack("background_8bit", 0.5);
 
+        window.onblur = function () {
+          audio.stopTrack();
+        };
+
+        window.onfocus = function () {
+          audio.playTrack("background_8bit", 0.5);
+        };
         // @ts-ignore
         state.change(state[key], false); // pls fix
         dispatch({
